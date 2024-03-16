8335167e37da72f2c6efe064a962aad8664f65c7
Juan Jose Abuin <juan.abuin@globant.com>
2024-03-16 18:44:54 -0300

    StartSaleScreen product cards

--- app/src/main/java/com/dshagapps/tfi_software/presentation/screen/StartSaleScreen.kt
+++ app/src/main/java/com/dshagapps/tfi_software/presentation/screen/StartSaleScreen.kt
@@ -1,20 +1,41 @@
 package com.dshagapps.tfi_software.presentation.screen
 
+import android.content.res.Resources.Theme
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import com.dshagapps.tfi_software.domain.entities.Stock
 import com.dshagapps.tfi_software.presentation.viewmodel.SaleViewModel
+import java.text.DecimalFormat
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StartSaleScreen(
     modifier: Modifier = Modifier,
@@ -25,25 +46,37 @@ fun StartSaleScreen(
 
     BackHandler(onBack = onBack)
 
+    var search by remember { mutableStateOf("") }
+
     val stockList = viewModel.stockList.collectAsState()
 
-    LazyColumn(
+    Column(
         modifier = modifier
-            .fillMaxWidth()
-            .padding(8.dp)
+            .fillMaxSize()
+            .padding(8.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        this.items(stockList.value) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(4.dp),
-                horizontalArrangement = Arrangement.Absolute.SpaceBetween
-            ) {
-                Text(text = "${it.id}")
-                Text(text = it.productDescription)
-                Text(text = it.colorDescription)
-                Text(text = it.sizeDescription)
-                Text(text = "Cantidad: ${it.quantity}")
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = search,
+            onValueChange = { search = it },
+            maxLines = 1
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 8.dp)
+        ) {
+            this.items(stockList.value) {
+                StockCard(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 4.dp),
+                    stock = it
+                )
             }
         }
     }
@@ -51,4 +84,69 @@ fun StartSaleScreen(
     LaunchedEffect(Unit) {
         viewModel.getStockByBranch(branchId)
     }
+}
+
+@Composable
+private fun StockCard(
+    modifier: Modifier = Modifier,
+    stock: Stock
+) {
+    Card(
+        modifier = modifier
+    ){
+        Column (modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)) {
+            StyledText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 4.dp),
+                title = "Prenda:",
+                description = stock.productDescription
+            )
+            StyledText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 2.dp),
+                title = "Color:",
+                description = stock.colorDescription
+            )
+
+            StyledText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 2.dp),
+                title = "Talle:",
+                description = stock.sizeDescription
+            )
+
+            StyledText(
+                modifier = Modifier.fillMaxWidth(),
+                title = "Precio:",
+                description = "$${DecimalFormat("#.00").format(stock.price)}"
+            )
+        }
+    }
+}
+
+
+@Composable
+private fun StyledText(
+    modifier: Modifier = Modifier,
+    title: String,
+    description: String
+) {
+
+    val combinedText = buildAnnotatedString {
+        append(title)
+        append(" ")
+        withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+            append(description)
+        }
+    }
+
+    Text(
+        modifier = modifier,
+        text = combinedText
+    )
 }
